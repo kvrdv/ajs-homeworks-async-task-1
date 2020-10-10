3065f95a3c0ed73e18d433ee18be35bb6bd51228
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-10-10 18:10:47 +0300

    first commit

---
+++ src/js/GameSavingLoader.js
@@ -0,0 +1,16 @@
+import read from './reader';
+import json from './parser';
+
+export default class GameSavingLoader {
+  static load() {
+    return new Promise((resolve) => {
+      const data = read();
+      data.then((response) => {
+        const value = json(response);
+        return value;
+      }).then((value) => {
+        resolve(value);
+      });
+    });
+  }
+}
